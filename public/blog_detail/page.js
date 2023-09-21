594d4d1aaa7ca54fe328ee6c12db4ee352ceba10
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-21 20:29:07 +0500

    'nextJsdesing'

---
+++ public/blog_detail/page.js
@@ -0,0 +1,59 @@
+export const blog = [
+    {
+        name:"Zeeshan",
+        class:"web and app",
+        timing:"5Pm to 6PM",
+        issueDate:new Date().toLocaleTimeString(),
+        image : 'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        name:"Zunair",
+        class:"web and app",
+        timing:"5Pm to 6PM",
+        issueDate:new Date().toLocaleTimeString(),
+        image : 'https://images.unsplash.com/photo-1480429370139-e0132c086e2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        name:"Faiz Naeem",
+        class:"web and app",
+        timing:"5Pm to 6PM",
+        issueDate:new Date().toLocaleTimeString(),
+        image : 'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        name:"Atta",
+        class:"web and app",
+        timing:"5Pm to 6PM",
+        issueDate:new Date().toLocaleTimeString(),
+        image : 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        name:"Junaid",
+        class:"web and app",
+        timing:"5Pm to 6PM",
+        issueDate:new Date().toLocaleTimeString(),
+        image : 'https://images.unsplash.com/photo-1581382575275-97901c2635b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        name:"Random",
+        class:"web and app",
+        timing:"5Pm to 6PM",
+        issueDate:new Date().toLocaleTimeString(),
+        image : 'https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        name:"Sahal",
+        class:"web and app",
+        timing:"5Pm to 6PM",
+        issueDate:new Date().toLocaleTimeString(),
+        image : 'https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        name:"Murtaza",
+        class:"web and app",
+        timing:"5Pm to 6PM",
+        issueDate:new Date().toLocaleTimeString(),
+        image : 'https://plus.unsplash.com/premium_photo-1666265087928-fe19db9887ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG1hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+
+]
--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { blog } from '../../../public/blog_detail/page'
 
 const page = () => {
-  return (
-    <div>
-      <h1>Blogs</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Blogs</h1>
+                <div className='flex justify-around items-center flex-wrap w-100'>
+            {
+                blog.map((user) => {
+                    return (
+                        <div className="card flex flex-col justify-center mt-5 shadow-lg p-3 rounded-md" style={{width: '18rem'}}>
+                            <img className="card-img-top rounded-lg" src={user.image} alt="Card image cap" style={{width : '300px' , height: '240px'}} />
+                            <div className="card-body">
+                                <h5 className="card-title">Name : {user.name}<span> {user.issueDate}</span></h5>
+                                <h6>Class : {user.class}</h6>
+                                <h6>Timing : {user.timing}</h6>
+                               <button className='rounded-md'>User_Data</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </div>
+    )
 }
 
 export default page
