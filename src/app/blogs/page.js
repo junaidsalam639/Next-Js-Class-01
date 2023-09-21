92167adda48051b86ad38f064809a28760024476
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-21 20:44:31 +0500

    'next__Jsx'

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -5,22 +5,22 @@ const page = () => {
     return (
         <div>
             <h1 className='text-center py-4 text-5xl font-bold underline'>Blogs</h1>
-                <div className='flex justify-around items-center flex-wrap w-100'>
-            {
-                blog.map((user) => {
-                    return (
-                        <div className="card flex flex-col justify-center mt-5 shadow-lg p-3 rounded-md" style={{width: '18rem'}}>
-                            <img className="card-img-top rounded-lg" src={user.image} alt="Card image cap" style={{width : '300px' , height: '240px'}} />
-                            <div className="card-body pt-5">
-                                <h5 className="card-title">Name : {user.name}<span> {user.issueDate}</span></h5>
-                                <h6>Class : {user.class}</h6>
-                                <h6>Timing : {user.timing}</h6>
-                               <button className='rounded-md'>User_Data</button>
+            <div className='flex justify-around items-center flex-wrap w-100'>
+                {
+                    blog.map((user) => {
+                        return (
+                            <div className="card flex flex-col justify-center mt-5 shadow-lg p-3 rounded-md" style={{ width: '18rem' }}>
+                                <img className="card-img-top rounded-lg" src={user.image} alt="Card image cap" style={{ width: '300px', height: '240px' }} />
+                                <div className="card-body pt-5 text-center">
+                                    <h5 className="card-title">Name : {user.name}<span> {user.issueDate}</span></h5>
+                                    <h6>Class : {user.class}</h6>
+                                    <h6>Timing : {user.timing}</h6>
+                                    <button className='rounded-md py-2 px-3 mt-1 bg-blue-100 hover:bg-blue-200'>User_Data</button>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })
-            }
+                        )
+                    })
+                }
             </div>
         </div>
     )
