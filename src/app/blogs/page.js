5b6118b68612be6a3bd245f252c3079a775dcd27
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-21 20:54:32 +0500

    'nextJsx$$'

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,13 +1,17 @@
-import React from 'react'
+'use client'
 import { blog } from '../../../public/blog_detail/page'
 
 const page = () => {
+    const userData = (e) => {
+        console.log(e);
+        
+    }
     return (
         <div>
             <h1 className='text-center py-4 text-5xl font-bold underline'>Blogs</h1>
             <div className='flex justify-around items-center flex-wrap w-100'>
                 {
-                    blog.map((user) => {
+                    blog.map((user , index) => {
                         return (
                             <div className="card flex flex-col justify-center mt-5 shadow-lg p-3 rounded-md" style={{ width: '18rem' }}>
                                 <img className="card-img-top rounded-lg" src={user.image} alt="Card image cap" style={{ width: '300px', height: '240px' }} />
@@ -15,7 +19,7 @@ const page = () => {
                                     <h5 className="card-title">Name : {user.name}<span> {user.issueDate}</span></h5>
                                     <h6>Class : {user.class}</h6>
                                     <h6>Timing : {user.timing}</h6>
-                                    <button className='rounded-md py-2 px-3 mt-1 bg-blue-100 hover:bg-blue-200'>User_Data</button>
+                                    <button className='rounded-md py-2 px-3 mt-1 bg-blue-100 hover:bg-blue-200' onClick={() => userData(index)}>User_Data</button>
                                 </div>
                             </div>
                         )
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
+'use client'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter();
+
+  const blog = () => {
+     router.push('/blogs')
+  }
   return (
     <>
       <div>
@@ -13,9 +20,7 @@ export default function Home() {
         <Link href={'/contact'}>
         <button className='py-2 px-3 my-3 mx-3 bg-green-300 text-blue-400 font-bold border-4'>Contact</button>
         </Link>
-        <Link href={'/blogs'}>
-        <button className='py-2 px-3 my-3 mx-3 bg-green-300 text-blue-400 font-bold border-4'>Blog</button>
-        </Link>
+        <button className='py-2 px-3 my-3 mx-3 bg-green-300 text-blue-400 font-bold border-4' onClick={blog}>Blog</button>
       </div>
     </>
   )
