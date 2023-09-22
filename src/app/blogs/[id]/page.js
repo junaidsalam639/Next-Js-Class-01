525ec7ac2ba6157d17c420b2d9ecfa459d2b0d8f
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-22 17:24:33 +0500

    'fast'

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -1,15 +1,27 @@
 'use client'
 
-export default function page({params}) {
+export default function page({ params }) {
   let name = localStorage.getItem('name');
   let class1 = localStorage.getItem('class');
   let timing = localStorage.getItem('timing');
   let image = localStorage.getItem('image');
   let detail = localStorage.getItem('detail');
-  console.log(name);
+  console.log(name, class1, timing, image, detail);
   return (
     <div>
       <h1>Id : {params.id}</h1>
+          <div className='flex justify-around items-center flex-wrap w-100'>
+            <div className="card flex flex-col justify-center mt-5 shadow-lg p-3 rounded-md" style={{ width: '100%' }}>
+              <img className="card-img-top rounded-lg" src={image} alt="Card image cap" style={{ width: '100%', height: '360px'}} />
+              <div className="card-body pt-5">
+                <h5 className="card-title"><b>Name : </b> {name}<span></span></h5>
+                <h6><b>Class</b> : {class1}</h6>
+                <h6><b>Timing</b> : {timing}</h6>
+                <h6><b>Detail</b> : {detail}</h6>
+                <button className='rounded-md py-2 px-3 mt-1 shadow-lg border-2'>User_Detail</button>
+              </div>
+            </div>
+          </div>
     </div>
   )
 }
