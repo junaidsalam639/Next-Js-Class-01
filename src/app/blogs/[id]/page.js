b923bc167c96ec4cc7de78c657d6e97f008e1f4d
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-22 17:05:39 +0500

    'slow'

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -1,9 +1,18 @@
-
+'use client'
 
 export default function page({params}) {
+  let name = localStorage.getItem('name');
+  let class1 = localStorage.getItem('class');
+  let timing = localStorage.getItem('timing');
+  let image = localStorage.getItem('image');
+  let detail = localStorage.getItem('detail');
+  console.log(name);
   return (
     <div>
       <h1>Id : {params.id}</h1>
     </div>
   )
 }
+
+
+// SEO stands for Search Engine Optimization. The goal of SEO is to create a strategy that will increase your rankings position in search engine results. The higher the ranking, the more organic traffic to your site, which ultimately leads to more business for you!
--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,14 +1,20 @@
 'use client'
 import { blog } from '../../../public/blog_detail/page'
 import { useRouter } from 'next/navigation'
+import './blogs.css'
 
 const page = () => {
     const router = useRouter();
 
     const userData = (e) => {
-        const copy = [...blog]
-        console.log(copy[e]);
-        router.push('/id')
+        const copy = [...blog];
+        let index = copy[e].name;
+        localStorage.setItem('name' , copy[e].name);
+        localStorage.setItem('class' , copy[e].class);
+        localStorage.setItem('timing' , copy[e].timing);
+        localStorage.setItem('image' , copy[e].image);
+        localStorage.setItem('detail' , copy[e].detail); 
+        router.push(`/blogs/${copy[e].name}`)
     }
     return (
         <div>
@@ -17,12 +23,12 @@ const page = () => {
                 {
                     blog.map((user , index) => {
                         return (
-                            <div className="card flex flex-col justify-center mt-5 shadow-lg p-3 rounded-md" style={{ width: '18rem' }}>
+                            <div key={index} className="card flex flex-col justify-center mt-5 shadow-lg p-3 rounded-md" style={{ width: '18rem' }}>
                                 <img className="card-img-top rounded-lg" src={user.image} alt="Card image cap" style={{ width: '300px', height: '240px' }} />
-                                <div className="card-body pt-5 text-center">
-                                    <h5 className="card-title">Name : {user.name}<span> {user.issueDate}</span></h5>
-                                    <h6>Class : {user.class}</h6>
-                                    <h6>Timing : {user.timing}</h6>
+                                <div className="card-body pt-5">
+                                    <h5 className="card-title"><b>Name : </b> {user.name}<span> {user.issueDate}</span></h5>
+                                    <h6><b>Class</b> : {user.class}</h6>
+                                    <h6><b>Timing</b> : {user.timing}</h6>
                                     <button className='rounded-md py-2 px-3 mt-1 shadow-lg border-2' onClick={() => userData(index)}>User_Detail</button>
                                 </div>
                             </div>
